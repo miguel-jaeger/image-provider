45a0a47970dc4df71b6275b097024997f48dad48
miguel-jaeger <miguel.jaeger@gmail.com>
2026-06-26 22:38:32 -0500

    feat: add ImageGrid responsive grid component

---
+++ app/src/components/dashboard/ImageGrid.tsx
@@ -0,0 +1,20 @@
+import type { Image } from '../../types/image'
+import { ImageCard } from './ImageCard'
+import { AddNewAssetCard } from './AddNewAssetCard'
+
+interface ImageGridProps {
+  images: Image[]
+  onDelete: (id: number) => void
+  onAddNew: () => void
+}
+
+export function ImageGrid({ images, onDelete, onAddNew }: ImageGridProps) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-gutter">
+      {images.map((image) => (
+        <ImageCard key={image.id} image={image} onDelete={onDelete} />
+      ))}
+      <AddNewAssetCard onClick={onAddNew} />
+    </div>
+  )
+}
